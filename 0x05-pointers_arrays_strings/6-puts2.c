2aa33040801eb7ff625304845858a6d8ff2722e7
myusername <myemail@gmail.com>
2023-03-27 13:34:09 -0700

    tasks

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,19 +7,20 @@
  */
 void puts2(char *str)
 {
-int len, i;
+	int len, i;
 
-len = 0;
+	len = 0;
 
-while (str[len] != 0)
-{
-len++;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
 }
 
-for (i = 0; i < len; i += 2)
-{
-_putchar(str[i]);
-}
-
-_putchar(n);
-}
